364015fa7b49c6cbf6acc49878cc8408af1cc623
Malcolm McFadden <malcolmyates34@gmail.com>
2020-11-11 23:28:03 -0500

    adding image to img

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac Bacon Cheese Burger');
